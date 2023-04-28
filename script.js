82b45dd59f42b0b76f34094b39ee868d693dcc4b
Danny Wittig <wittigdaniel.g@gmail.com>
2023-04-28 18:02:32 -0500

    Fixing merge conflicts

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // in the html.
 $(function () {
   var today = dayjs();
-
   // var currentHour = dayjs().hour()
   var currentHour = 12;
   var firstHour = 8;
@@ -18,18 +17,6 @@ $(function () {
     })
   })
   
-
-
-  var currentHour = dayjs().hour()
-  var firstHour = 8;
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
 
   $('.time-block').each(function(index){
     var checkHour = index + firstHour;
@@ -47,13 +34,6 @@ $(function () {
     $(this).val(localStorage.getItem(storageText));
   })
 
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
   $('#currentDay').text(today.format('MMMM DD, YYYY'));
 });
